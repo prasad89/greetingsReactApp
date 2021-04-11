6f987ced876dae3df8555084c05730d1b7903d28
Prasad Bhalerao <67261499+Prasad89@users.noreply.github.com>
2021-04-11 19:04:07 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+let currDate = new Date();
+currDate = currDate.getHours();
+let greeting = "";
+const cssStyle = {};
+
+if (currDate >= 5 && currDate < 12) {
+  greeting = "Good Morning"
+  cssStyle.color = "green";
+}
+else if (currDate >= 12 && currDate < 15) {
+  greeting = "Good Noon";
+  cssStyle.color = "red";
+}
+else if (currDate >= 15 && currDate < 17) {
+  greeting = "Good Afternoon";
+  cssStyle.color = "orange";
+}
+else if (currDate >= 17 && currDate < 20) {
+  greeting = "Good Evening";
+  cssStyle.color = "gray";
+}
+else {
+  greeting = "Good Night";
+  cssStyle.color = "black";
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <>
+    <div>
+      <h1> Hello, <span style={cssStyle}> {greeting} </span> </h1>
+    </div>
+  </>,
+  document.getElementById("root"),
+)
